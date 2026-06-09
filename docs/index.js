48905728cd6a97f3f353fc24f21e33e1f6e0f279
ics-kitagawa <kitagawa@ics-web.jp>
2026-06-09 16:29:43 +0900

    searchの実装をdotLottie-webにする。最適化をdestroyのみにする

--- docs/index.js
+++ docs/index.js
@@ -1,9 +1,11 @@
+import { DotLottie } from "https://cdn.jsdelivr.net/npm/@lottiefiles/dotlottie-web@0.74.0/+esm";
+
 document.addEventListener("DOMContentLoaded", () => {
   const lottieContainer = document.querySelector("#lottie");
 
-  const anim = lottie.loadAnimation({
-    container: lottieContainer,
-    path: "./assets/search.json",
+  const anim = new DotLottie({
+    canvas: lottieContainer,
+    src: "./assets/search.lottie",
     autoplay: false, // 自動再生はしない（デフォルトはtrue。省略可）
   });
 
@@ -11,42 +13,35 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // テキストボックス関連の要素
   const button = document.querySelector("#search-button");
-  const result = document.querySelector("#search-result");
   const statusText = document.querySelector("#search-status");
 
   // 虫眼鏡ボタン押下時、lottieを再生する
   button.addEventListener("click", async () => {
-    result.classList.add("isLoading");
-    statusText.innerText = "再検索中…";
-    await onSearch(anim, result, statusText);
+    statusText.innerText = "検索中…";
+    await onSearch(anim);
   });
   button.addEventListener("keydown", async (event) => {
     if (event.key === "Enter") {
-      result.classList.add("isLoading");
-      statusText.innerText = "再検索中…";
-      await onSearch(anim, result, statusText);
+      statusText.innerText = "検索中…";
+      await onSearch(anim);
     }
   });
 
-  // アニメーションの再生が完了したらローディング画面を非表示にする
-  // （本来はアニメーションの完了を待たず優先線的に表示させるべきですが作例としてcompleteイベントを使用しています）
   anim.addEventListener("complete", () => {
-    result.classList.remove("isLoading");
-    statusText.innerText = "結果";
-    result.classList.remove("isFirstView");
+    statusText.innerText = "検索完了";
   });
 });
 
 const onSearch = async (anim) => {
   // アニメーション再生
-  anim.loop = true; // ループ再生
+  anim.setLoop(true); // ループ再生
   anim.play();
 
   // ローディング風にランダムな時間待機
   await wait(Math.random() * 5000);
 
   // アニメーションがループしないよう設定を変更（再生中のアニメーションが完了したら自動で停止）
-  anim.loop = false;
+  anim.setLoop(false);
 };
 
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
